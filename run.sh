48ea12b5c780a15483cee0cf9d6d6116b9016ae5
ykhudorozhkov <y.khudorozhkov@g.nsu.ru>
2023-03-05 15:21:01 +0700

    Edited run.sh script

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+#!/bin/sh
 DIR="./target/"
 FILE="./target/jdu-1.0-SNAPSHOT-jar-with-dependencies.jar"
 if [ ! -d "$DIR" ]; then
         mvn clean package
-        java -jar ./target/jdu-1.0-SNAPSHOT-jar-with-dependencies.jar
+        java -jar ./target/jdu-1.0-SNAPSHOT-jar-with-dependencies.jar $@
 else
         if [ -e "$FILE" ]; then
-                java -jar ./target/jdu-1.0-SNAPSHOT-jar-with-dependencies.jar
+                java -jar ./target/jdu-1.0-SNAPSHOT-jar-with-dependencies.jar $@
         else
                 mvn clean package
-                java -jar ./target/jdu-1.0-SNAPSHOT-jar-with-dependencies.jar
+                java -jar ./target/jdu-1.0-SNAPSHOT-jar-with-dependencies.jar $@
         fi
 fi
